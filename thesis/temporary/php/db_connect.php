0d051f8c661d7a54b0472937850c2ba207324856
Tewodros Ewunetu <teddy3bewunetu@gmail.com>
2020-12-01 11:31:43 +0300

    first commit

--- thesis/temporary/php/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_host="localhost";
-$db_database="iot";
-$db_username="root";
-$db_password="";
-$mysqli= new mysqli($db_host, $db_username, $db_password, $db_database);
-if($mysqli->connect_errno)
-die ("failed");
-?>
